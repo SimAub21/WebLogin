f20fffb8e216b0114b719baab72ff6fe170505fe
BuildTools <simreiaub5@gmail.com>
2021-11-18 20:20:00 +0100

    inisialise project

---
+++ WebLogin/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebLogin.Models;
+
+namespace WebLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult GetLoginData(LoginModel loginModel)
+        {
+            string username = loginModel.Username;
+            string password = loginModel.Password;
+            return View("~/Views/Login/Sucess.cshtml", loginModel);
+        }
+    }
+}
